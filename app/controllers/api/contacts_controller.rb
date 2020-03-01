d6375c4fa482f2be4ad7a4864d5e3fad9e8d8acf
Biodun <biodun9@gmail.com>
2020-03-01 01:14:30 +0100

    rubocop linting

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -2,36 +2,37 @@
 
 module Api
   class ContactsController < ApplicationController
+    
     def index
       contacts = Contact.order('created_at DESC')
       render json: { status: 'SUCCESS', message: 'Loaded Contacts', data: contacts }, status: :ok
     end
 
-    def show 
-        contact = Contact.find(params[:id])
-        render json: { status: 'SUCCESS', message: 'Loaded Contact', data: contact }, status: :ok
+    def show
+      contact = Contact.find(params[:id])
+      render json: { status: 'SUCCESS', message: 'Loaded Contact', data: contact }, status: :ok
     end
 
-    def create 
-        contact = Contact.new(contact_params)
+    def create
+      contact = Contact.new(contact_params)
 
-        if contact.save
-            render json: { status: 'SUCCESS', message: 'Saved Contact', data: contact }, status: :ok
-        else 
-            render json: { status: 'ERROR', message: 'Contact not saved', data: contact.errors }, status: :unprocessable_entry
-        end
+      if contact.save
+        render json: { status: 'SUCCESS', message: 'Saved Contact', data: contact }, status: :ok
+      else
+        render json: { status: 'ERROR', message: 'Contact not saved', data: contact.errors }, status: :unprocessable_entry
+      end
     end
 
-    def destroy 
-        contact = Contact.find(params[:id])
-        contact.destroy
-        render json: { status: 'SUCCESS', message: 'Contact deleted', data: contact }, status: :ok
+    def destroy
+      contact = Contact.find(params[:id])
+      contact.destroy
+      render json: { status: 'SUCCESS', message: 'Contact deleted', data: contact }, status: :ok
     end
 
+    private
 
-    private 
-    def contact_params 
-        params.permit(:FirstName, :LastName, :PhoneNumber, :email)
+    def contact_params
+      params.permit(:FirstName, :LastName, :PhoneNumber, :email)
     end
   end
 end
